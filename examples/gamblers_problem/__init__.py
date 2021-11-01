8e66c544c9eba8e9e377c51362477054fbb663af
sjshin0505 <36652309+sjshin0505@users.noreply.github.com>
2021-11-01 22:59:15 +0900

    Added gambler's problem example package.

---
+++ examples/gamblers_problem/__init__.py
@@ -0,0 +1,10 @@
+# Author: Seungjae Shin <sjshin0505@{etri.re.kr, gmail.com}>
+
+"""Package for simulating gambler's problem described in the following reference:
+R. S. Sutton and A. G. Barto, Reinforcement Learning - An Introduction, 2nd ed., 2018 (Example 4.3: Gambler's Problem).
+"""
+
+import gym
+from examples.gamblers_problem.gym_env import GamblersProblem
+
+gym.register(id='GamblersProblem-v0', entry_point='examples.gamblers_problem:GamblersProblem')
