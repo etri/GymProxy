c7b356ef5b6536de8de042cb22a91d01115d409d
sjshin0505 <36652309+sjshin0505@users.noreply.github.com>
2021-11-25 22:48:59 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+"""Setup for installation.
+"""
+
+from setuptools import setup
+
+setup(name='gymproxy',
+      version='1.0.0',
+      author='Seungjae Shin',
+      author_email='sjshin0505@{etri.re.kr, gmail.com}',
+      description=('A python package for porting an external python-based simulation on OpenAI Gym environment using '
+                   'multi-threading.'),
+      license='BSD',
+      packages=['gymproxy'],
+      install_requires=['numpy', 'gym'])
