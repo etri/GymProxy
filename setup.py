f12eeb7aa3408baea1c7ed7c8851f856b1bf2fd1
labry <justin.labry@gmail.com>
2024-07-26 22:08:58 +0900

    o3 option

--- setup.py
+++ setup.py
@@ -1,28 +1,32 @@
-"""Setup for installation.
-"""
-
-from distutils.core import setup
+from distutils.core import setup, Extension
 from Cython.Build import cythonize
-from distutils.extension import Extension
 
-cyslow = Extension(
-    "acelerate",
-    sources=['./gymproxy/env_proxy_cy.pyx', './gymproxy/base_actual_env_cy.pyx'],
-    extra_compile_args=['-O3'],
-    extra_link_args=['-O3']
-)
+extensions = [
+    Extension(
+        "gymproxy.env_proxy_cy",
+        ["./gymproxy/env_proxy_cy.pyx"],
+        extra_compile_args=["-O3"],
+        extra_link_args=["-O3"]
+    ),
+    Extension(
+        "gymproxy.base_actual_env_cy",
+        ["./gymproxy/base_actual_env_cy.pyx"],
+        extra_compile_args=["-O3"],
+        extra_link_args=["-O3"]
+    )
+]
 
-setup(name='gymproxy',
-      version='1.0.0',
-      author='Sae Hyong Park and Seungjae Shin',
-      author_email='{labry,sjshin0505}@etri.re.kr',
-      description=('A python package for porting an external python-based simulation on OpenAI Gym environment using '
-                   'multi-threading.'),
-      license='BSD',
-      packages=['gymproxy'],
-      install_requires=['numpy', 'gymnasium==0.29.1'],
-      ext_modules=cythonize([cyslow],
-      compiler_directives={
+setup(
+    name='gymproxy',
+    version='1.0.0',
+    author='Sae Hyong Park and Seungjae Shin',
+    author_email='{labry,sjshin0505}@etri.re.kr',
+    description=('A python package for porting an external python-based simulation on OpenAI Gym environment using '
+                 'multi-threading.'),
+    license='BSD',
+    packages=['gymproxy'],
+    install_requires=['numpy', 'gymnasium==0.29.1'],
+    ext_modules=cythonize(extensions, compiler_directives={
         'cdivision': False,
         'boundscheck': False,
         'wraparound': False,
@@ -30,4 +34,4 @@ setup(name='gymproxy',
         'initializedcheck': False,
         'embedsignature': True
     })
-      )
+)
