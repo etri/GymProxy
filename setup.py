e6c8466e07fa86e7b79b9e9d01271bc3ff4356b7
labry <justin.labry@gmail.com>
2024-07-26 21:48:08 +0900

    gcc O3 option

--- setup.py
+++ setup.py
@@ -3,6 +3,14 @@
 
 from distutils.core import setup
 from Cython.Build import cythonize
+from distutils.extension import Extension
+
+cyslow = Extension(
+    "acelerate",
+    sources=['./gymproxy/env_proxy_cy.pyx', './gymproxy/base_actual_env_cy.pyx'],
+    extra_compile_args=['-O3'],
+    extra_link_args=['-O3']
+)
 
 setup(name='gymproxy',
       version='1.0.0',
@@ -13,7 +21,8 @@ setup(name='gymproxy',
       license='BSD',
       packages=['gymproxy'],
       install_requires=['numpy', 'gymnasium==0.29.1'],
-      ext_modules=cythonize(['./gymproxy/base_env_cy.pyx','./gymproxy/env_proxy_cy.pyx', './gymproxy/base_actual_env_cy.pyx'], compiler_directives={
+      ext_modules=cythonize([cyslow],
+      compiler_directives={
         'cdivision': False,
         'boundscheck': False,
         'wraparound': False,
