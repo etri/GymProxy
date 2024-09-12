250814068e0d87f2dbd0f22a6d51482ec2338a6c
sjshin0505 <36652309+sjshin0505@users.noreply.github.com>
2024-09-12 15:48:38 +0900

    Update env_proxy.py

--- gymproxy/env_proxy.py
+++ gymproxy/env_proxy.py
@@ -69,7 +69,7 @@ class EnvProxy(ABC):
             self._lock.acquire()
 
             self._actual_env_event.wait()   # Waits for calling get_obs() method from the gym-type environment.
-            self._reset_actual_env(self._actual_env, seed, options)   # Actually resets the actual environment.
+            self._actual_env.run(seed, options)    # Actually resets the actual environment.
 
         # Locks the critical section for making reset_actual_env_() stopped before resetting the actual environment. It
         # will be unlocked by get_obs() method calling from the gym-type environment.
