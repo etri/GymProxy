03e5055bf3cf73c6ff3b588c8b82fd1252c2ac59
sjshin0505 <36652309+sjshin0505@users.noreply.github.com>
2024-09-25 18:19:43 +0900

    Update actual_env.py

--- examples/access_control_q/actual_env.py
+++ examples/access_control_q/actual_env.py
@@ -7,15 +7,14 @@ Queuing Task).
 """
 
 import logging
-import random
-from typing import Optional
-
 import numpy as np
+import random
+import examples.access_control_q
 
+from typing import Optional
 from gymproxy.actual_env import ActualEnv, TerminateGymProxy
 
 logger = logging.getLogger('access_control_q_actual_env')
-
 
 class AccessControlQueueActualEnv(ActualEnv):
     """External environment class that actually simulates the access-control queuing task.
@@ -24,12 +23,14 @@ class AccessControlQueueActualEnv(ActualEnv):
     def __init__(self, kwargs: Optional[dict] = None):
         """Constructor.
 
-        :param kwargs: Dictionary of keyword arguments. It should have 'config' argument that is a dictionary for
-        setting configuration parameters. kwargs['config'] should define following keys:
-            num_steps (int): Number of time-steps.
-            num_servers (int): Number of servers.
-            server_free_probability (float): Probability that a server completes its task.
-            priorities (list of float): Priorities that a customer can has.
+        Args:
+            kwargs: Dictionary of keyword arguments. 
+            It should have 'config' argument that is a dictionary for setting configuration parameters. 
+            kwargs['config'] should define following keys:
+                num_steps (int): Number of time-steps.
+                num_servers (int): Number of servers.
+                server_free_probability (float): Probability that a server completes its task.
+                priorities (list of float): Priorities that a customer can has.
         """
         env_proxy = kwargs['env_proxy']
         ActualEnv.__init__(self, env_proxy)
@@ -40,12 +41,12 @@ class AccessControlQueueActualEnv(ActualEnv):
         self._priorities = kwargs['priorities']
         self._reward = 0.
         self._t = 0
-        # self._cnt = 0
 
     def run(self, seed_:int, kwargs: Optional[dict] = None):
         """Runs the access-control queuing task environment.
 
-        :param kwargs: Dictionary of keyword arguments.
+        Args:
+            kwargs: Dictionary of keyword arguments.
         """
         try:
             obs = None
@@ -56,18 +57,11 @@ class AccessControlQueueActualEnv(ActualEnv):
             self._reward = 0.
             self._t = 0
             while self._t < self._num_steps:
+                # Assumes that a new customer arrives. Chooses new customer's priority from the list of candidate priorities.
+                priority = get_new_customer(self._priorities)
 
-                # Assumes that a new customer arrives. Chooses new customer's priority from the list of candidate
-                # priorities.
-                priority = self._priorities[random.randrange(0, len(self._priorities))]
-
-                # Identifies free servers.
-                free_servers = list(filter(lambda i_:
-                                           self._server_states[i_] == 'free', range(0, len(self._server_states))))
-
-                # Observation consists of customer's priority and number of free servers.
-                obs = np.array([priority, len(free_servers)], dtype=np.int32)
-
+                free_servers = get_free_servers(self._server_states)    # Identifies free servers.
+                obs = make_obs(priority, free_servers)    # Observation consists of customer's priority and number of free servers.
                 terminated = False
                 truncated = False
                 info = {}
@@ -77,31 +71,25 @@ class AccessControlQueueActualEnv(ActualEnv):
 
                 if len(free_servers) > 0:
                     if action:  # Means acceptance.
-
-                        # Randomly chooses a server for the customer among free ones.
-                        i = free_servers[random.randrange(0, len(free_servers))]
+                        i = choose_free_server(free_servers)    # Randomly chooses a server for the customer among free ones.
                         self._server_states[i] = 'busy'     # Selected server becomes busy.
-
-                        self._reward = priority* 0.005     # Reward is the priority of accepted customer.
+                        self._reward = priority * 0.005     # Reward is the priority of accepted customer.
                     else:   # Means rejection.
                         self._reward = 0.
                 else:   # Rejects the customer if the number of free servers is 0.
                     self._reward = 0.
-                busy_servers = list(filter(lambda i_:
-                                           self._server_states[i_] == 'busy', range(0, len(self._server_states))))
-
+                busy_servers = get_busy_servers(self._server_states)
+                
                 # Busy servers become free with _server_free_probability.
                 for i in busy_servers:
                     if random.random() < self._server_free_probability:
                         self._server_states[i] = 'free'
 
                 self._t += 1
-                # self._cnt += 1
 
             # Arrives to the end of the episode (terminal state).
-            free_servers = list(filter(lambda i_:
-                                       self._server_states[i_] == 'free', range(0, len(self._server_states))))
-            obs = np.array([0, len(free_servers)], dtype=np.int32)
+            free_servers = get_free_servers(self._server_states)
+            obs = make_obs(0, free_servers)
             terminated = True
             truncated = True
             #info = {}
