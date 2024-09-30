01b6293a0b9531d37772ca0aa69442a8cb1f5721
sjshin0505 <36652309+sjshin0505@users.noreply.github.com>
2024-09-30 10:03:17 +0900

    Update actual_env.py

--- examples/car_rental/actual_env.py
+++ examples/car_rental/actual_env.py
@@ -1,171 +1,70 @@
-# Author: Sae Hyong Park <labry@etri.re.kr>
-# Author: Seungjae Shin <sjshin0505@{etri.re.kr, gmail.com}>
+# Author: Sae Hyong Park <labry@etri.re.kr>, Seungjae Shin <sjshin0505@{etri.re.kr, gmail.com}>
 
-"""External environment for actually simulating Jack's car rental example. It is implemented based on the following
-reference:
+"""Gym-type environment for simulating Jack's car rental example. It is implemented based on the following reference:
 R. S. Sutton and A. G. Barto, Reinforcement Learning - An Introduction, 2nd ed., 2018 (Example 4.2: Jack's Car Rental).
 """
-
 import logging
 from typing import Optional
 
+import gymnasium
 import numpy as np
+from gymnasium.spaces import Box, Discrete, Tuple
 
-from gymproxy.actual_env import ActualEnv, TerminateGymProxy
+from examples.car_rental.actual_env import CarRentalActualEnv
+from gymproxy import GymEnv
 
-logger = logging.getLogger('car_rental_actual_env')
+logger = logging.getLogger('car_rental')
 
-
-class CarRentalActualEnv(ActualEnv):
-    """External environment class that actually simulates Jack's car rental.
+class CarRental(GymEnv):
+    """Class defining observation and action spaces of gym-type CarRental environment.
     """
-
-    def __init__(self, kwargs: Optional[dict] = None):
+    def __init__(self, config: Optional[dict] = None):
         """Constructor.
 
-        :param kwargs: Dictionary of keyword arguments. It should have 'config' argument that is a dictionary for
-        setting configuration parameters. kwargs['config'] should define following keys:
-            num_steps (int): Number of time-steps.
-            max_num_cars_per_loc (int): Maximum number of cars per location.
-            rental_credit (float): Credit for each rental.
-            car_moving_cost (float): Cost for each car moving between the two locations.
-            rental_rate_0 (int): Mean arrival rate of rental request at the location 0 for each time-steps.
-            rental_rate_1 (int): Mean arrival rate of rental request at the location 1 for each time-steps.
-            return_rate_0 (int): Mean arrival rate of return at the location 0 for each time-steps.
-            return_rate_1 (int): Mean arrival rate of return at the location 1 for each time-steps.
-        """
-        env_proxy = kwargs.get('env_proxy')
-        ActualEnv.__init__(self, env_proxy)
-        config = kwargs.get('config')
-        self._num_steps = kwargs['num_steps']
-        self._max_num_cars_per_loc = kwargs['max_num_cars_per_loc']
-        self._rental_credit = kwargs['rental_credit']
-        self._car_moving_cost = kwargs['car_moving_cost']
-        self._lambda_rental_0 = kwargs['rental_rate_0']
-        self._lambda_rental_1 = kwargs['rental_rate_1']
-        self._lambda_return_0 = kwargs['return_rate_0']
-        self._lambda_return_1 = kwargs['return_rate_1']
-        self._available_cars = [self._max_num_cars_per_loc] * 2
-        self._reward = 0.
-        self._t = 0
-
-    def run(self, seed_:int, kwargs: Optional[dict] = None):
-        """Runs Jack's car rental environment.
-
         :param kwargs: Dictionary of keyword arguments.
         """
-        try:
-            obs = None
-            terminated = False
-            truncated = False
-            info = {}
-            np.random.seed(seed_)
-            self._reward = 0.
-            self._t = 0
-            self._available_cars = [self._max_num_cars_per_loc] * 2
+        NUM_STEPS = 100
+        MAX_NUM_CARS_PER_LOC = 20
+        RENTAL_CREDIT = 10.
+        CAR_MOVING_COST = 2.
+        RENTAL_RATE_0 = 4
+        RENTAL_RATE_1 = 3
+        RETURN_RATE_0 = 3
+        RETURN_RATE_1 = 2
 
-            while self._t < self._num_steps and not terminated:
+        if config is None:
+            logger.info("config is None.")
+            config = {'num_steps': NUM_STEPS,
+                      'max_num_cars_per_loc': MAX_NUM_CARS_PER_LOC,
+                      'rental_credit': RENTAL_CREDIT,
+                      'car_moving_cost': CAR_MOVING_COST,
+                      'rental_rate_0': RENTAL_RATE_0,
+                      'rental_rate_1': RENTAL_RATE_1,
+                      'return_rate_0': RETURN_RATE_0,
+                      'return_rate_1': RETURN_RATE_1}
 
-                if self._t != 0:
-                    action = CarRentalActualEnv.get_action(obs, self._reward, terminated, truncated, info)
-                else:
-                    action = (0, np.array([0]))
+        GymEnv.actual_env_class = CarRentalActualEnv
+        super().__init__(config)
 
-                msg = None
-
-                # New rental requests arrive at two locations for car rental. The arrival rates follow Poisson
-                # distribution.
-                n_req_0 = np.random.poisson(self._lambda_rental_0)
-                n_req_1 = np.random.poisson(self._lambda_rental_1)
-
-                # Identifies the number of possible rentals and reward.
-                n_rentals = min(self._available_cars[0], n_req_0) + min(self._available_cars[1], n_req_1)
-                self._reward = n_rentals * self._rental_credit * 0.005
-
-                # Checks if there is out of car for the two locations
-                if self._available_cars[0] < n_req_0:
-                #    msg = 'The business is lost because no available car at location 0'
-                    self._reward = 0
-                    terminated = True
-                    truncated = True
-                if self._available_cars[1] < n_req_1:
-                    self._reward = 0
-                    # if terminated:
-                    #     msg += ' and location 1'
-                    # else:
-                        #msg = 'The business is lost because no available car at location 1'
-                    terminated = True
-                    truncated = True
-                # if terminated:
-                #     msg += '.'
-
-                self._available_cars[0] = max(self._available_cars[0] - n_req_0, 0)
-                self._available_cars[1] = max(self._available_cars[1] - n_req_1, 0)
-
-                # A number of notes in the information dictionary.
-                info['rental_requests'] = [n_req_0, n_req_1]
-                # if msg:
-                #     info['msg'] = msg
-                # elif 'msg' in info.keys():
-                #     del info['msg']
-
-                # Checks if the business is lost.
-                if terminated:
-                    continue
-
-                # Some rented cars are returned. The return rates follow Poisson distribution. Note that the number of
-                # available cars at each location should not be exceed _max_num_cars_per_loc.
-                # np.random.seed(1)
-                n_return_0 = np.random.poisson(self._lambda_return_0)
-                n_return_1 = np.random.poisson(self._lambda_return_1)
-                self._available_cars[0] = min(self._available_cars[0] + n_return_0, self._max_num_cars_per_loc)
-                self._available_cars[1] = min(self._available_cars[1] + n_return_1, self._max_num_cars_per_loc)
-
-                info['returns'] = [n_return_0, n_return_1]  # Note returns in the information dictionary.
-
-                # Observation consists of the numbers of available cars at the two locations.
-
-                obs = np.array(self._available_cars, dtype=np.int32)
-
-                # logger.info("self._available_cars[0] {} - n_moving {}".format(self._available_cars[0], n_moving))
-                # logger.info("self._available_cars[1] {} - n_moving {}".format(self._available_cars[1], n_moving))
-                # Action consists of source location, from which cars should be moved, and number of cars to be moved.
-                # where action takes place
-                # print("action:", action)
-                # print("type:", type(action))
-                src = action[0]
-                dst = 1 - src
-                n_moving = action[1].item()
-                # Moves cars from the source location to the destination location. Note that the number of available
-                # cars at each location should not be exceed _max_num_cars_per_loc.
-                self._available_cars[src] = max(self._available_cars[src] - n_moving, 0)
-                self._available_cars[dst] = min(self._available_cars[dst] + n_moving, self._max_num_cars_per_loc)
-
-                self._reward = self._reward - (n_moving * 0.005 * 2)
-
-
-                self._t += 1
-
-            # Arrives to the end of the episode (terminal state).
-            terminated = True
-            truncated = True
-            CarRentalActualEnv.set_obs_and_reward(obs, self._reward, terminated, truncated, info)
-
-        # Exception handling block.
-        except TerminateGymProxy:
-            # Means termination signal triggered by the agent.
-            logger.info('Terminating CarRental environment.')
-            ActualEnv.env_proxy.terminate_sync()
-            exit(1)
-        except Exception as e:
-            logger.exception(e)
-            ActualEnv.env_proxy.terminate_sync()
-            exit(1)
-
-    def finish(self, kwargs: Optional[dict] = None):
-        """Finishes Jack's car rental environment.
+    @staticmethod
+    def build_obs_space(kwargs: Optional[dict] = None) -> gymnasium.Space:
+        """Builds observation space.
 
         :param kwargs: Dictionary of keyword arguments.
+        :return: Observation space.
         """
-        logger.info("finish")
-        return
+        config = kwargs
+        max_num_cars_per_loc = config.get('max_num_cars_per_loc', 20)
+        return Box(low=0, high=max_num_cars_per_loc, shape=(2,), dtype=np.int_)
+
+    @staticmethod
+    def build_action_space(kwargs: Optional[dict] = None) -> gymnasium.Space:
+        """Builds action space.
+
+        :param kwargs: Dictionary of keyword arguments.
+        :return: Action space.
+        """
+        config = kwargs
+        # max_num_cars_per_loc = config.get('max_num_cars_per_loc')
+        max_num_cars_for_move = 5
+        return Tuple((Discrete(2), Box(low=0, high=max_num_cars_for_move, shape=(1,), dtype=np.int_)))
