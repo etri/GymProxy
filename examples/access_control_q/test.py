f4624fca218ae9f2c3466df64e86bc19f419ea88
sjshin0505 <36652309+sjshin0505@users.noreply.github.com>
2024-09-30 10:00:13 +0900

    Update test.py

--- examples/access_control_q/test.py
+++ examples/access_control_q/test.py
@@ -1,15 +1,14 @@
-# Author: Sae Hyong Park <labry@etri.re.kr>
-# Author: Seungjae Shin <sjshin0505@{etri.re.kr, gmail.com}>
+# Author: Sae Hyong Park <labry@etri.re.kr>, Seungjae Shin <sjshin0505@{etri.re.kr, gmail.com}>
 
 """Testing script for AccessControlQueue environment implemented based on the following reference:
-R. S. Sutton and A. G. Barto, Reinforcement Learning - An Introduction, 2nd ed., 2018 (Example 10.2: An Access-Control
-Queuing Task).
+R. S. Sutton and A. G. Barto, Reinforcement Learning - An Introduction, 2nd ed., 2018 (Example 10.2: An Access-Control Queuing Task).
 """
 
 import logging
-import numpy as np
 
+from examples.utilities import get_step_log_str
 from examples.access_control_q import *
+
 
 # Setting logger
 FORMAT = "[%(asctime)s|%(levelname)s|%(name)s] %(message)s"
@@ -34,49 +33,21 @@ def main():
               'num_servers': NUM_SERVERS,
               'server_free_probability': SERVER_FREE_PROB,
               'priorities': PRIORITIES}
-
     env = gym.make(id='AccessControlQueue-v0', config=config)
-    accumulated_result = 0
+
     for i in range(0, NUM_EPISODES):
         j = 0
-        obs, info = env.reset(seed=126, options={})
-        logger.info(str(obs)+str(info))
+        obs, info = env.reset(seed=2024, options={})
         while True:
-            env.render()
             action = env.action_space.sample()  # Means random agent.
             obs, reward, terminated, truncated, info = env.step(action)
-            accumulated_result += reward
-
-            log_step(i, j, obs, reward, terminated, info, action)
+            step_log_str = get_step_log_str(i, j, obs, reward, terminated, truncated, info, action)
+            logger.info(step_log_str)
             j = j + 1
-            if terminated:
+            if terminated or truncated:
                 break
 
-    logger.info("Accumulated result: " + str(accumulated_result))
     env.close()
-
-
-def log_step(episode: int, step: int, obs: np.ndarray, reward: float, done: bool, info: dict, action: int):
-    """Utility function for printing logs.
-
-    :param episode: Episode number.
-    :param step: Time-step.
-    :param obs: Observation of the current environment.
-    :param reward: Reward from the current environment.
-    :param done: Indicates whether the episode ends or not.
-    :param info: Contains auxiliary diagnostic information (helpful for debugging, and sometimes learning).
-    :param action: An action provided by the agent.
-    """
-    priority = obs[0].item()
-    num_free_servers = obs[1].item()
-    step_str = '{}-th step in {}-th episode / '.format(step, episode)
-    obs_str = 'obs: {} / '.format((priority, num_free_servers))
-    reward_str = 'reward: {} / '.format(reward)
-    done_str = 'done: {} / '.format(done)
-    info_str = 'info: {} / '.format(info)
-    action_str = 'action: {}'.format(True if action else False)
-    result_str = step_str + obs_str + reward_str + done_str + info_str + action_str
-    logger.info(result_str)
 
 
 if __name__ == "__main__":
